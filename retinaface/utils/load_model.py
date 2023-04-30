6186e9026f9ea9ad345d83123f9a4b6c2ca063ab
chatflip <oo.chat.flip@gmail.com>
2023-04-30 20:25:06 +0900

    add verbose option

--- retinaface/utils/load_model.py
+++ retinaface/utils/load_model.py
@@ -1,25 +1,28 @@
 import torch
 
-def check_keys(model, pretrained_state_dict):
+def check_keys(model, pretrained_state_dict, verbose):
     ckpt_keys = set(pretrained_state_dict.keys())
     model_keys = set(model.state_dict().keys())
     used_pretrained_keys = model_keys & ckpt_keys
     unused_pretrained_keys = ckpt_keys - model_keys
     missing_keys = model_keys - ckpt_keys
-    print('Missing keys:{}'.format(len(missing_keys)))
-    print('Unused checkpoint keys:{}'.format(len(unused_pretrained_keys)))
-    print('Used keys:{}'.format(len(used_pretrained_keys)))
+    if verbose:
+        print('Missing keys:{}'.format(len(missing_keys)))
+        print('Unused checkpoint keys:{}'.format(len(unused_pretrained_keys)))
+        print('Used keys:{}'.format(len(used_pretrained_keys)))
     assert len(used_pretrained_keys) > 0, 'load NONE from pretrained checkpoint'
     return True
 
-def remove_prefix(state_dict, prefix):
+def remove_prefix(state_dict, prefix, verbose=False):
     ''' Old style model is stored with all names of parameters sharing common prefix 'module.' '''
-    print('remove prefix \'{}\''.format(prefix))
+    if verbose:
+        print('remove prefix \'{}\''.format(prefix))
     f = lambda x: x.split(prefix, 1)[-1] if x.startswith(prefix) else x
     return {f(key): value for key, value in state_dict.items()}
 
-def load_model(model, pretrained_path, load_to_cpu):
-    print('Loading pretrained model from {}'.format(pretrained_path))
+def load_model(model, pretrained_path, load_to_cpu, verbose=False):
+    if verbose:
+        print('Loading pretrained model from {}'.format(pretrained_path))
     if load_to_cpu:
         pretrained_dict = torch.load(pretrained_path, map_location=lambda storage, loc: storage)
     else:
@@ -29,7 +32,7 @@ def load_model(model, pretrained_path, load_to_cpu):
         pretrained_dict = remove_prefix(pretrained_dict['state_dict'], 'module.')
     else:
         pretrained_dict = remove_prefix(pretrained_dict, 'module.')
-    check_keys(model, pretrained_dict)
+    check_keys(model, pretrained_dict, verbose)
     model.load_state_dict(pretrained_dict, strict=False)
     return model
 
